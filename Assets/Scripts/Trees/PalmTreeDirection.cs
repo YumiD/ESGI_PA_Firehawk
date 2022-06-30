a7f1e9351128fb5ccdeb8b7402854f2aaf611d14
YumiD <gacon.dorian@gmail.com>
2022-06-30 17:17:18 +0200

    Direction Palm Tree fix Warning

---
+++ Assets/Scripts/Trees/PalmTreeDirection.cs
@@ -0,0 +1,15 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class PalmTreeDirection : MonoBehaviour
+{
+    public void Start()
+    {
+        transform.DOMoveY(transform.position.y - .5f, .7f).SetEase(Ease.Linear).SetLoops(-1, LoopType.Yoyo);
+    }
+
+    private void OnDestroy()
+    {
+        transform.DOKill(true);
+    }
+}
